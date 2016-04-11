fee0be867399e89f24a29d28b2521fc86b843ae0
Jianxin Ma <majx13fromthu@gmail.com>
2016-04-11 21:54:03 +0800

    before changing compiler

--- src/compiler.c
+++ src/compiler.c
@@ -26,16 +26,6 @@ enum {
 };
 // clang-format on
 
-// system calls
-// clang-format off
-enum {
-    S_fork=1, S_exit,   S_wait,   S_pipe,   S_write,  S_read,   S_close,  S_kill,
-    S_exec,   S_open,   S_mknod,  S_unlink, S_fstat,  S_link,   S_mkdir,  S_chdir,
-    S_dup2,   S_getpid, S_sbrk,   S_sleep,  S_uptime, S_lseek,  S_mount,  S_umount,
-    S_socket, S_bind,   S_listen, S_poll,   S_accept, S_connect,
-};
-// clang-format on
-
 typedef unsigned char uchar;
 typedef unsigned short ushort;
 typedef unsigned int uint;
