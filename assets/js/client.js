2e06e1b6c140ed5aa22bf6eb4177f7ce161b08b5
Jianxin Ma <majx13fromthu@gmail.com>
2016-04-13 17:57:30 +0800

    github

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,26 +1,34 @@
+/*jslint white:true browser:true */
+/*global CodeMirror Github */
+
+"use strict";
+
 (function() {
-    //    var socket = io('http://localhost:8080/');
-    //    socket.on('connect', function() {
-    //        socket.send('hi');
-    //        socket.on('message', function(msg) {});
-    var editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
+    var editor, terminal, github, repo;
+
+    editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
         mode: "text/x-csrc",
         styleActiveLine: true,
         lineNumbers: true,
         gutters: ["CodeMirror-linenumbers", "breakpoints"]
     });
-    editor.on("gutterClick", function(cm, n) {
-        var info = cm.lineInfo(n);
-        cm.setGutterMarker(n, "breakpoints", info.gutterMarkers ? null : makeMarker());
+    editor.on("gutterClick", function(cm, ln) {
+        var marker;
+
+        if (cm.lineInfo(ln).gutterMarkers) {
+            cm.setGutterMarker(ln, "breakpoints", null);
+        } else {
+            marker = document.createElement("div");
+            marker.style.color = "gray";
+            marker.innerHTML = "●";
+            cm.setGutterMarker(ln, "breakpoints", marker);
+        }
     });
 
-    function makeMarker() {
-        var marker = document.createElement("div");
-        marker.style.color = "gray";
-        marker.innerHTML = "●";
-        return marker;
-    }
-    var terminal = CodeMirror.fromTextArea(document.getElementById("terminal"), {
+    terminal = CodeMirror.fromTextArea(document.getElementById("terminal"), {
         readOnly: true
     });
+
+    github = new Github();
+    repo = github.getRepo("JianixnMa", "v9.js");
 })();
